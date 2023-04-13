cb874e55d1010a52708a5634f383b87b8be3df58
Patricio Osterno Rios <patricio.osterno1@gmail.com>
2023-04-13 18:57:36 -0300

    Adicionar arquivos de projeto.

---
+++ ModeloAnemico1.cs
@@ -0,0 +1,44 @@
+﻿namespace ModeloAnemico1
+{
+    public class ModeloAnemico1
+    {
+        public sealed class Cliente
+        {
+            public int Id { get; private set; }
+            public string Nome { get; private set; }
+            public string Endereco { get; private set; }
+
+            public Cliente(int id, string nome, string endereco)
+            {
+                Validar(id, nome, endereco);
+                Id = id;
+                Nome = nome;
+                Endereco = endereco;
+            }
+
+            public void Update(int id, string nome,string endereco) 
+            {
+                Validar(id, nome, endereco);
+                Id = id;
+                Nome = nome;
+                Endereco = endereco;
+            }
+
+            private void Validar(int id, string nome, string endereco)
+            {
+                if (id < 0)
+                    throw new InvalidOperationException("O id tem quje ser maior que 0");
+                
+                if (nome.Length< 3) 
+                    throw new ArgumentException("O nome deve ter pelo menos 3 caracteres");
+                
+                if (nome.Length > 100)
+                    throw new ArgumentException("O nome deve ter no máximo 100 caracteres");
+                
+                if (string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(endereco))
+                    throw new InvalidOperationException($"{(nome == null ? "Nome": "Endereco")} não pode ser nulo");
+                
+            }
+        }
+    }
+}
